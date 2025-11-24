19833bd3e8083b84459dc01679067948aaa98b0b
Abdelilah El mobariki <elmobariki.abdlilah@gmail.com>
2025-11-24 20:48:58 +0100

    message

---
+++ Day_01/alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void print_alphabet(void)
+{
+  printf("a b c d e f g h i j k l m n o p q r s t u v w x y z");
+}
+int main()
+{
+    print_alphabet();
+    return 0;
+}
---
+++ Day_01/char.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void print_char(void)
+{
+  printf("a");
+}
+int main()
+{
+    print_char();
+    return 0;
+}
